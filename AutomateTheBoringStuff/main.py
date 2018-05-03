352942fe6bea687c3ab33c9440793739c233d081
Richard Gonzales <richardgonzales@outlook.com>
2018-05-02 22:56:02 -0700

    kept messing around in chapter 2 near figure 2-13

--- AutomateTheBoringStuff/main.py
+++ AutomateTheBoringStuff/main.py
@@ -1,11 +1,10 @@
-name = 'Dracula'
-age = 4000
-if name == 'Alice':
-    print('Hi, Alice.')
-elif age < 12:
-    print('You are not Alice, kiddo') 
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
-elif age > 100:
-    print('You are not Alice, grannie')
-
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'swordfish':
+        break
+print('Access Granted')
